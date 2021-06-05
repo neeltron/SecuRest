48e1dfa3cf5fcf842a8567e685c18fb7a6b889b2
unknown <500075823@stu.upes.ac.in>
2021-06-06 01:36:46 +0530

    Added Iteration Counter

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 
+iter = 0
 cam = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cam.read()
     cv2.imshow('frame', frame)
+    iter += 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite()
         break
     
     
