d07d4f9a5353e38374bd22dd6f63c6a7700145cb
unknown <500075823@stu.upes.ac.in>
2021-06-06 01:15:30 +0530

    Switching to OpenCV

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from picamera import PiCamera
-from time import sleep
-
-cam = PiCamera()
-
-cam.start_preview()
-sleep(5)
-for i in range(0, 5):
-    cam.capture(str(i)+".jpg")
-    sleep(1)
-    
-cam.stop_preview()
+import cv2
