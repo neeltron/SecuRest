efcc7b313857bcfd735eddca215e1f859c8389ff
unknown <500075823@stu.upes.ac.in>
2021-06-06 10:59:39 +0530

    Added an HC-SR04 Sensor

--- SecuRest/SecuRest.ino
+++ SecuRest/SecuRest.ino
@@ -1,7 +1,19 @@
-void setup() {
+int distance = 0;
+long duration = 0;
 
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  digitalWrite(3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(3, LOW);
+  duration = pulseIn(2, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.println(distance);
 }
